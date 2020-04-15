a822829836722c2def21a4a1c76df9793833f8d0
AngusTheBeef <54658992+AngusTheBeef@users.noreply.github.com>
2020-04-14 19:59:22 -0600

    Initial commit

---
+++ tests/tst_projtest.cpp
@@ -0,0 +1,35 @@
+#include <QtTest>
+
+// add necessary includes here
+
+class projtest : public QObject
+{
+    Q_OBJECT
+
+public:
+    projtest();
+    ~projtest();
+
+private slots:
+    void test_case1();
+
+};
+
+projtest::projtest()
+{
+
+}
+
+projtest::~projtest()
+{
+
+}
+
+void projtest::test_case1()
+{
+
+}
+
+QTEST_APPLESS_MAIN(projtest)
+
+#include "tst_projtest.moc"
